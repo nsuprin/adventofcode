2342f46734381a18320508f54a9594021bfab9be
nicsupri <nicsupri@publicisgroupe.net>
2020-12-12 22:48:49 +0000

    Day 12 part 2

--- d12/run.php
+++ d12/run.php
@@ -779,7 +779,6 @@ INPUT;
   public $iEast = 0, $iNorth = 0;
   private $iDeltaNorth  = 0;
   private $iDeltaEast  = 1;
-  private $iDirection   = 0;
   private $aDirection = [0 => 'E', 90 => 'S', 180 => 'W', 270 => 'N'];
   private $iCap = 0;
 
@@ -808,7 +807,7 @@ INPUT;
 
   public static function test2() {
       $aInput = explode("\n", self::$sTest);
-      if (26 == (self::parse2($aInput))) {
+      if (286 == (self::parse2($aInput))) {
           var_dump('OK');
       } else {
           var_dump('KO');
@@ -838,9 +837,8 @@ INPUT;
   }
 
   public function setDirection($sDirection, $iValue) {
-      if (in_array($sDirection, $this->aDirection)) {
-          $iDirection = array_search($sDirection, $this->aDirection);
-          switch ($this->aDirection[$iDirection]) {
+
+          switch ($sDirection) {
               case 'N':
                   $this->iNorth += $iValue;
                   break;
@@ -853,11 +851,6 @@ INPUT;
               case 'W' :
                   $this->iEast -= $iValue;
                   break;
-              default:
-                  throw new Exception('soucis de direction coco');
-          }
-      } else {
-          switch ($sDirection) {
               case 'L':
                   $iValue = -1 * $iValue;
               case 'R':
@@ -890,7 +883,6 @@ INPUT;
               default;
                   throw new Exception('souci de direction');
           }
-      }
   }
 
     /**
@@ -916,7 +908,117 @@ INPUT;
    * @return mixed
    */
   public static function parse2($aInput) {
-
+      $oRun = new Ship();
+      $oRun->navigate($aInput);
+      var_dump([abs($oRun->iEast), abs($oRun->iNorth)]);
+      return abs($oRun->iEast) + abs($oRun->iNorth);
   }
 
+}
+
+abstract  class MetaWaypoint {
+    public $iEast = 0, $iNorth = 0;
+}
+class Ship extends MetaWaypoint {
+
+    /**
+     * @var Waypoint
+     */
+    private $waypoint;
+
+    public function __construct()
+    {
+        $this->waypoint = new Waypoint(10, 1);
+    }
+
+    public function navigate($aInput) {
+        foreach ($aInput as $iLine => $sLine) {
+            if (preg_match('/([NSEWLRF])(\d+)/', $sLine, $aMatches)) {
+                list(, $sDirection, $iValue)    = $aMatches;
+                $this->setDirection($sDirection, $iValue);
+                var_dump(implode(" ", [$sDirection, $iValue,
+                    ' ship: ', $this->iEast, 'e ', $this->iNorth, 'n ',
+                    'waypoint', $this->waypoint->iEast, 'e, ', $this->waypoint->iEast]));
+            } else {
+                throw new Exception("line not found");
+            }
+        }
+    }
+
+    public function setDirection($sDirection, $iValue) {
+        switch ($sDirection) {
+            case 'N':
+                $this->waypoint->iNorth += $iValue;
+                break;
+            case 'S' :
+                $this->waypoint->iNorth -= $iValue;
+                break;
+            case 'E' :
+                $this->waypoint->iEast += $iValue;
+                break;
+            case 'W' :
+                $this->waypoint->iEast -= $iValue;
+                break;
+            case 'L':
+            case 'R':
+                $this->waypoint->rotate($sDirection, $iValue);
+                break;
+            case 'F':
+                $this->iEast += $this->waypoint->iEast * $iValue;
+                $this->iNorth += $this->waypoint->iNorth * $iValue;
+                break;
+            default;
+                throw new Exception('souci de direction');
+        }
+
+    }
+}
+class Waypoint extends MetaWaypoint {
+
+    public function __construct($iEast, $iNorth)
+    {
+        $this->iEast    = $iEast;
+        $this->iNorth   = $iNorth;
+    }
+
+    public function rotate($sWise, $iValue) {
+        $iEast  = $iNorth = 0;
+        switch ($sWise) {
+            case 'R' :
+                switch ($iValue) {
+                    case 90:
+                        $iEast    = $this->iNorth;
+                        $iNorth   = -1 * $this->iEast;
+                        break;
+                    case 180:
+                        $iEast    = -1 * $this->iEast;
+                        $iNorth   = -1 * $this->iNorth;
+                        break;
+                    case 270:
+                        $iEast    = -1 * $this->iNorth;
+                        $iNorth   = $this->iEast;
+                        break;
+                }
+                break;
+            case 'L' :
+                switch ($iValue) {
+                    case 90:
+                        $iEast    = -1 * $this->iNorth;
+                        $iNorth   = $this->iEast;
+                        break;
+                    case 180:
+                        $iEast    = -1 * $this->iEast;
+                        $iNorth   = -1 * $this->iNorth;
+                        break;
+                    case 270:
+                        $iEast    = $this->iNorth;
+                        $iNorth   = -1 * $this->iEast;
+                        break;
+                }
+                break;
+
+        }
+        $this->iEast    = $iEast;
+        $this->iNorth   = $iNorth;
+    }
 }
