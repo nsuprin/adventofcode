78b3df4a6e004279f8ab6081774a8fabd428d584
nicsupri <nicsupri@publicisgroupe.net>
2020-12-13 22:00:09 +0000

    Day 13 part 2

--- d13/run.php
+++ d13/run.php
@@ -35,8 +35,39 @@ INPUT;
     }
 
   public static function test2() {
+
+      $aInput = explode("\n", "0\n17,x,13,19");
+      if (3417 == (self::parse2($aInput))) {
+          var_dump('OK');
+      } else {
+          var_dump('KO');
+      }
+      $aInput = explode("\n", "0\n67,7,59,61");
+      if (754018 == (self::parse2($aInput))) {
+          var_dump('OK');
+      } else {
+          var_dump('KO');
+      }
+      $aInput = explode("\n", "0\n67,x,7,59,61");
+      if (779210 == (self::parse2($aInput))) {
+          var_dump('OK');
+      } else {
+          var_dump('KO');
+      }
+      $aInput = explode("\n", "0\n67,7,x,59,61");
+      if (1261476 == (self::parse2($aInput))) {
+          var_dump('OK');
+      } else {
+          var_dump('KO');
+      }
+      $aInput = explode("\n", "0\n1789,37,47,1889");
+      if (1202161486 == (self::parse2($aInput))) {
+          var_dump('OK');
+      } else {
+          var_dump('KO');
+      }
       $aInput = explode("\n", self::$sTest);
-      if (286 == (self::parse2($aInput))) {
+      if (1068781 == (self::parse2($aInput))) {
           var_dump('OK');
       } else {
           var_dump('KO');
@@ -71,128 +102,27 @@ INPUT;
    * @return mixed
    */
   public static function parse2($aInput) {
-      list($iErliestTimestamp, $sBuses) = $aInput;
-      $aBuses   = explode(',', str_replace('x,', '', $sBuses));
-      var_dump([$iErliestTimestamp, $aBuses]);
-      asort($aBuses);
-      $bContinue    = true;
-      $iTimestamp   = $iErliestTimestamp;
-      do {
-          foreach ($aBuses as $iBus) {
-              if (0 == $iTimestamp % $iBus) {
-                  $bContinue    = false;
-                  break 2;
-              }
+      list(, $sBuses) = $aInput;
+      $aBuses   = [];
+      foreach (explode(',', $sBuses) as $iDelta => $iBus) {
+          if ('x' == $iBus) {
+              continue;
           }
-          $iTimestamp++;
-      } while ($bContinue);
+          $aBuses[$iDelta]  = $iBus;
+      }
+
+      $iStepSize    = array_values($aBuses)[0];
+
+      unset($aBuses[array_keys($aBuses)[0]]);
+      $iCurrentTimestamp    = 0;
+
+      foreach ($aBuses as $iDeltabus => $iBus) {
+          while (0 == ($iCurrentTimestamp + $iDeltabus) || 0 != ($iCurrentTimestamp + $iDeltabus) % $iBus) {
+              $iCurrentTimestamp    += $iStepSize;
+          }
+          $iStepSize    *= $iBus;
+      }
+      return $iCurrentTimestamp;
   }
 
-}
-
-abstract  class MetaWaypoint {
-    public $iEast = 0, $iNorth = 0;
-}
-class Ship extends MetaWaypoint {
-
-    /**
-     * @var Waypoint
-     */
-    private $waypoint;
-
-    public function __construct()
-    {
-        $this->waypoint = new Waypoint(10, 1);
-    }
-
-    public function navigate($aInput) {
-        foreach ($aInput as $iLine => $sLine) {
-            if (preg_match('/([NSEWLRF])(\d+)/', $sLine, $aMatches)) {
-                list(, $sDirection, $iValue)    = $aMatches;
-                $this->setDirection($sDirection, $iValue);
-                var_dump(implode(" ", [$sDirection, $iValue,
-                    ' ship: ', $this->iEast, 'e ', $this->iNorth, 'n ',
-                    'waypoint', $this->waypoint->iEast, 'e, ', $this->waypoint->iEast]));
-            } else {
-                throw new Exception("line not found");
-            }
-        }
-    }
-
-    public function setDirection($sDirection, $iValue) {
-        switch ($sDirection) {
-            case 'N':
-                $this->waypoint->iNorth += $iValue;
-                break;
-            case 'S' :
-                $this->waypoint->iNorth -= $iValue;
-                break;
-            case 'E' :
-                $this->waypoint->iEast += $iValue;
-                break;
-            case 'W' :
-                $this->waypoint->iEast -= $iValue;
-                break;
-            case 'L':
-            case 'R':
-                $this->waypoint->rotate($sDirection, $iValue);
-                break;
-            case 'F':
-                $this->iEast += $this->waypoint->iEast * $iValue;
-                $this->iNorth += $this->waypoint->iNorth * $iValue;
-                break;
-            default;
-                throw new Exception('souci de direction');
-        }
-
-    }
-}
-class Waypoint extends MetaWaypoint {
-
-    public function __construct($iEast, $iNorth)
-    {
-        $this->iEast    = $iEast;
-        $this->iNorth   = $iNorth;
-    }
-
-    public function rotate($sWise, $iValue) {
-        $iEast  = $iNorth = 0;
-        switch ($sWise) {
-            case 'R' :
-                switch ($iValue) {
-                    case 90:
-                        $iEast    = $this->iNorth;
-                        $iNorth   = -1 * $this->iEast;
-                        break;
-                    case 180:
-                        $iEast    = -1 * $this->iEast;
-                        $iNorth   = -1 * $this->iNorth;
-                        break;
-                    case 270:
-                        $iEast    = -1 * $this->iNorth;
-                        $iNorth   = $this->iEast;
-                        break;
-                }
-                break;
-            case 'L' :
-                switch ($iValue) {
-                    case 90:
-                        $iEast    = -1 * $this->iNorth;
-                        $iNorth   = $this->iEast;
-                        break;
-                    case 180:
-                        $iEast    = -1 * $this->iEast;
-                        $iNorth   = -1 * $this->iNorth;
-                        break;
-                    case 270:
-                        $iEast    = $this->iNorth;
-                        $iNorth   = -1 * $this->iEast;
-                        break;
-                }
-                break;
-
-        }
-        $this->iEast    = $iEast;
-        $this->iNorth   = $iNorth;
-    }
 }
