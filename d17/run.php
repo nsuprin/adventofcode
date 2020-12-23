4a1f0f3603bb7ec4ccd185c8540305b7ea98adda
nicsupri <nicsupri@publicisgroupe.net>
2020-12-23 21:21:40 +0000

    Day 17 part 2

--- d17/run.php
+++ d17/run.php
@@ -35,7 +35,7 @@ class D17
     public static function test2()
     {
         $aInput = explode("\n", self::$sTest);
-        if (12 * 11 * 13 == (self::parse2($aInput))) {
+        if (848 == (self::parse2($aInput))) {
             var_dump('OK');
         } else {
             var_dump('KO');
@@ -60,7 +60,11 @@ class D17
      */
     public static function parse2($aInput)
     {
-
+        $oEnergy    = new EnergySource4D($aInput);
+        for ($i = 1 ; $i <= 6 ; $i ++) {
+            $oEnergy->runCycle();
+        }
+        return sizeof($oEnergy->aCubes);
     }
 
 
@@ -97,26 +101,23 @@ class EnergySource {
      * New Cubes
      * @var array
      */
-    private $aNewCubes  = [];
+    protected $aNewCubes  = [];
 
     /**
-     * @var int[]
+     * Neighbors cubes
+     * @var array
      */
-    private $aDimensions    = [
-        'max' => ['x' => 0, 'y' => 0, 'z' => 0],
-        'min' => ['x' => 0, 'y' => 0, 'z' => 0],
-    ];
+    protected $aNeighbors   = [];
 
     /**
      * EnergySource constructor.
      * @param $aInput
      */
     public function __construct($aInput) {
-        $z  = 0;
         foreach ($aInput as $y => $sLine) {
             foreach (str_split($sLine) as $x => $sCube) {
                 if (Cube::ACTIVE == $sCube) {
-                    $oCube  = new Cube($x, $y, $z);
+                    $oCube  = $this->initCube($x, $y);
                     $this->addCube($oCube);
                 }
             }
@@ -124,20 +125,19 @@ class EnergySource {
     }
 
     /**
+     * @param $x
+     * @param $y
+     * @return Cube
+     */
+    protected function initCube($x, $y) {
+        return Cube::getInstance($x, $y);
+    }
+
+    /**
      * @param Cube $oCube
      */
-    private function addCube(Cube $oCube) {
+    protected function addCube(Cube $oCube) {
         $this->aCubes[(string)$oCube]   = $oCube;
-        foreach ($this->aDimensions ['min'] as $sDimension => $iValue) {
-            if ($oCube->{$sDimension} <= $this->aDimensions['min'][$sDimension]) {
-                $this->aDimensions['min'][$sDimension] = $oCube->{$sDimension} - 1;
-            }
-        }
-        foreach ($this->aDimensions ['max'] as $sDimension => $iValue) {
-            if ($this->aDimensions['max'][$sDimension] <= $oCube->{$sDimension}) {
-                $this->aDimensions['max'][$sDimension] = $oCube->{$sDimension} + 1;
-            }
-        }
     }
 
     /**
@@ -148,31 +148,29 @@ class EnergySource {
         foreach ($this->aCubes as $oCube) {
             $this->cubeCycle($oCube);
         }
-        $this->aCubes   = [];
-        foreach ($this->aNewCubes as $oNewCube) {
-            $this->addCube($oNewCube);
+        foreach ($this->aNeighbors as $oNeighbor) {
+            $this->activeCube($oNeighbor);
         }
+        $this->aCubes   = $this->aNewCubes;
     }
 
     /**
      * @param Cube $oCube
      */
-    private function cubeCycle(Cube $oCube) {
+    protected function cubeCycle(Cube $oCube) {
         if (in_array(sizeof($oCube->getActiveNeighbors($this->aCubes)), [2, 3])) {
-            $this->aNewCubes[]    = $oCube;
+            $this->aNewCubes[(string)$oCube]    = $oCube;
         }
-        $aNeighbors = $oCube->getNeighborPositions();
-        foreach ($aNeighbors as $oNeighbor) {
-            $this->activeCube($oNeighbor, $aNeighbors);
-        }
+
+        $this->aNeighbors += $oCube->getNeighborPositions();
     }
 
     /**
      * @param Cube $oNeighbor
      */
-    private function activeCube(Cube $oNeighbor) {
+    protected function activeCube(Cube $oNeighbor) {
         if (3 === sizeof($oNeighbor->getActiveNeighbors($this->aCubes))) {
-            $this->aNewCubes[]  = $oNeighbor;
+            $this->aNewCubes[(string)$oNeighbor]  = $oNeighbor;
         }
     }
 }
@@ -180,11 +178,33 @@ class EnergySource {
 class Cube  {
     const ACTIVE  = '#';
 
-    private $aPositions = ['x' => 0, 'y' => 0, 'z' => 0];
+    protected $aPositions = ['x' => 0, 'y' => 0, 'z' => 0];
 
-    private $aNeighbors = [];
+    protected $aNeighbors = [];
 
-    public function __construct($x = 0, $y = 0, $z = 0) {
+    private static $aInstances    = [];
+
+    /**
+     * @param int $x
+     * @param int $y
+     * @param int $z
+     * @return Cube|mixed
+     */
+    public static function getInstance($x = 0, $y = 0, $z = 0) {
+        $sKey   = implode(',', [$x, $y, $z]);
+        if (!isset(self::$aInstances[$sKey])) {
+            self::$aInstances[$sKey]    = new self($x, $y, $z);
+        }
+        return self::$aInstances[$sKey];
+    }
+
+    /**
+     * Cube constructor.
+     * @param int $x
+     * @param int $y
+     * @param int $z
+     */
+    private function __construct($x = 0, $y = 0, $z = 0) {
         $this->x        = $x;
         $this->y        = $y;
         $this->z        = $z;
@@ -225,3 +245,77 @@ class Cube  {
         return array_intersect_assoc($aCube, $this->getNeighborPositions());
     }
 }
+
+class EnergySource4D extends EnergySource {
+
+    public function __construct($aInput) {
+        parent::__construct($aInput);
+    }
+
+    /**
+     * @param $x
+     * @param $y
+     * @return Cube
+     */
+    protected function initCube($x, $y) {
+        return Cube4D::getInstance($x, $y);
+    }
+}
+
+
+class Cube4D extends Cube {
+    private static $aInstances    = [];
+
+    /**
+     * @param int $x
+     * @param int $y
+     * @param int $z
+     * @param int $w
+     * @return Cube|mixed
+     */
+    public static function getInstance($x = 0, $y = 0, $z = 0, $w = 0) {
+        $sKey   = implode(',', [$x, $y, $z, $w]);
+        if (!isset(self::$aInstances[$sKey])) {
+            self::$aInstances[$sKey]    = new self($x, $y, $z, $w);
+        }
+        return self::$aInstances[$sKey];
+    }
+    /**
+     * Cube4D constructor.
+     * @param int $x
+     * @param int $y
+     * @param int $z
+     * @param int $w
+     */
+    private function __construct($x = 0, $y = 0, $z = 0, $w = 0)
+    {
+        $this->x        = $x;
+        $this->y        = $y;
+        $this->z        = $z;
+        $this->w        = $w;
+    }
+
+    /**
+     * @return array
+     */
+    public function getNeighborPositions() {
+        $this->aNeighbors = [];
+        if (empty($this->aNeighbors)) {
+            for ($x = $this->x - 1 ; $x <= $this->x + 1 ; $x++) {
+                for ($y = $this->y - 1 ; $y <= $this->y + 1 ; $y++) {
+                    for ($z = $this->z - 1; $z <= $this->z + 1; $z++) {
+                        for ($w = $this->w - 1; $w <= $this->w + 1; $w++) {
+                            if ($x == $this->x && $y == $this->y && $z == $this->z && $w == $this->w) {
+                                continue;
+                            }
+                            $oNeighbors = new self($x, $y, $z, $w);
+                            $this->aNeighbors[(string)$oNeighbors] = $oNeighbors;
+                        }
+                    }
+                }
+            }
+        }
+        return $this->aNeighbors;
+    }
+}
+
