5631632a9091d165cf35c374a4d18fcd81cf734d
nicsupri <nicsupri@publicisgroupe.net>
2021-12-07 17:35:58 +0000

    Day 07 improve run, and process test2 & run2

--- d07/run.php
+++ d07/run.php
@@ -3,6 +3,10 @@
 
 class D07 extends Day
 {
+  /**
+   * @var array
+   */
+  private $aFuelMax = [];
 
   /**
    * @inheritDoc
@@ -12,11 +16,10 @@ class D07 extends Day
     list($sCrabs)  = $aInput;
     $aCrabs  = explode(',', $sCrabs);
     asort($aCrabs);
-    $iMeanCrab  = floor($this->getMedian($aCrabs));
     $aConsos = [];
-    for ($i = 0 ; $i < sizeof($aCrabs) / 2 ; $i++) {
-      //$aConsos[$iMeanCrab + $i] = $this->getFuelConso($aCrabs, $iMeanCrab + $i);
-      $aConsos[$iMeanCrab + $i * -1] = $this->getFuelConso($aCrabs, $iMeanCrab + $i * -1);
+    $aConsos = [];
+    foreach ($aCrabs as $i => $aCrab) {
+      $aConsos[$i] = $this->getFuelConso($aCrabs, $i);
     }
     asort($aConsos);
     return array_values($aConsos)[0];
@@ -38,42 +41,34 @@ class D07 extends Day
   }
 
   /**
-   * A PHP function that will calculate the median value
-   * of an array
-   *
-   * @param array $arr The array that you want to get the median value of.
-   * @return boolean|float|int
-   * @throws Exception If it's not an array
+   * @param $aCrabs
+   * @param $iMeanCrab
+   * @return float|int
    */
-  private function getMedian($arr) {
-    //Make sure it's an array.
-    if(!is_array($arr)){
-      throw new Exception('$arr must be an array!');
+  private function getFuelConso2($aCrabs, $iMeanCrab) {
+    $iFuel  = 0;
+    foreach ($aCrabs as $iCrab) {
+      $iFuelCrab = abs($iMeanCrab - $iCrab);
+
+      //echo "$iCrab to $iMeanCrab : $iFuelCrab\n";
+      $iFuel += $this->getFuelMax($iFuelCrab);
     }
-    //If it's an empty array, return FALSE.
-    if(empty($arr)){
-      return false;
+
+    return $iFuel;
+  }
+
+  /**
+   * @param $iFuel
+   */
+  private function getFuelMax($iFuel)
+  {
+    if (!isset($this->aFuelMax[$iFuel])) {
+      $this->aFuelMax[$iFuel] = 0;
+      for ($i = 1; $i <= $iFuel; $i++) {
+        $this->aFuelMax[$iFuel] += $i;
+      }
     }
-    //Count how many elements are in the array.
-    $num = count($arr);
-    //Determine the middle value of the array.
-    $middleVal = floor(($num - 1) / 2);
-    //If the size of the array is an odd number,
-    //then the middle value is the median.
-    if($num % 2) {
-      return $arr[$middleVal];
-    }
-    //If the size of the array is an even number, then we
-    //have to get the two middle values and get their
-    //average
-    else {
-      //The $middleVal var will be the low
-      //end of the middle
-      $lowMid = $arr[$middleVal];
-      $highMid = $arr[$middleVal + 1];
-      //Return the average of the low and high.
-      return (($lowMid + $highMid) / 2);
-    }
+    return $this->aFuelMax[$iFuel];
   }
 
   /**
@@ -81,7 +76,15 @@ class D07 extends Day
    */
   public function parse2($aInput)
   {
-    // TODO: Implement parse2() method.
+    list($sCrabs)  = $aInput;
+    $aCrabs  = explode(',', $sCrabs);
+    asort($aCrabs);
+    $aConsos = [];
+    foreach ($aCrabs as $i => $aCrab) {
+      $aConsos[$i] = $this->getFuelConso2($aCrabs, $i);
+    }
+    asort($aConsos);
+    return array_values($aConsos)[0];
   }
 
   /**
@@ -97,6 +100,6 @@ class D07 extends Day
    */
   protected function getExpectedTest2()
   {
-    // TODO: Implement getExpectedTest2() method.
+    return 168;
   }
 }
